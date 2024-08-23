49e6bf90f49ad699df850e08325d28422afb0a07
deluxxe01 <dudaklincarvalho@gmail.com>
2024-08-23 17:15:49 -0300

    Criação do primeiro projeto com React bonitão

---
+++ Hello World/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1>O Tempo e seus Ecos</h1>
+   
+    <p>O tempo, esse velho contador de histórias, avança silencioso, levando consigo a cadência de nossos dias e o murmúrio das nossas esperanças. A cada manhã, o sol se levanta com a promessa de um novo começo, enquanto as sombras da noite recuam, deixando para trás as lembranças do ontem.</p>
+    </>
+  )
+}
+
+export default App
