de392313ebd43ca3253bc4c0b1efdef3fb00a5bf
deluxxe01 <dudaklincarvalho@gmail.com>
2024-08-29 17:17:54 -0300

    Atualizado

---
+++ componentes/src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Receita Brownie</h1>
+      <h2>Como fazer</h2>
+      <p>Comando usado: ul>li*5  </p>
+      <ul>
+        <li>atalho: Para apagar uma linha inteira: CTRL + SHIFT + K </li>
+        <li>atalho: Para comentar: CTRL + ;</li>
+        <li>atalho: Para abrir a tela de emojis: Windows + . </li>
+        <li>atalho: Para subir uma linha: alt + setinhas + </li>
+        <li>atalho: Para abri o cmd pelo VScode:  CTRL + J ou CTRL +  </li>
+      </ul>
+      <p>Para esfregar varios cursores pela tela(digitar varias coisas ao mesmo tempo): Clica com a rodinha e esfrega ela pela tela.</p>
+     
+     <p>Extensões: </p>
+     <ul>
+      <li>ES7 React/Redux/GraphQL</li>
+      <li>Auto Rename Tag</li>
+      <li>Color High</li>
+      <li>Power Mode</li>
+      <li>Dracula Theme</li>
+     </ul>
+
+    </>
+  )
+}
+
+export default App
