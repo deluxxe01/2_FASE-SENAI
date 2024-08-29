909d604127ec9584c0e7008da7b2216efee15583
deluxxe01 <dudaklincarvalho@gmail.com>
2024-08-29 17:21:54 -0300

    Atualizado

---
+++ ComponentesDeVerdade/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Cabecalho from './components/Cabecalho'
+import Links from './components/Links'
+function App() {
+  
+
+  return (
+    <>
+     <Cabecalho />
+     <Links />
+     
+    </>
+  )
+}
+
+export default App
---
+++ ComponentesDeVerdade/src/components/Links.jsx
@@ -0,0 +1,9 @@
+function Links() {
+    return(
+
+        <div>
+            <p>Link 1 | Link 2 | Link 3 | Link 4</p>
+        </div>
+    )
+}
+export default Links
